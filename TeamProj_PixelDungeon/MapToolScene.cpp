28ebbb3519e770572de9444cf5b96b5d368150e4
YOOJOOWON <sauna59479@gmail.com>
2017-11-20 05:59:26 +0900

    맵툴 제작 시작

--- TeamProj_PixelDungeon/MapToolScene.cpp
+++ TeamProj_PixelDungeon/MapToolScene.cpp
@@ -12,6 +12,12 @@ MapToolScene::~MapToolScene()
 }
 HRESULT MapToolScene::init()
 {
+	IMAGEMANAGER->addFrameImage("mapTiles", "tile.bmp", 0, 0, 512, 288, 16, 9, true, RGB(255, 0, 255));
+
+
+
+
+	_homeButton = RectMake(WINSIZEX - 120, WINSIZEY - 60, WINSIZEX - 20, WINSIZEY - 20);
 
 	return S_OK;
 }
@@ -22,10 +28,50 @@ void MapToolScene::release()
 void MapToolScene::update()	
 {
 
+
+
+
 }
+
+
+
+void MapToolScene::buttonInput()
+{
+
+
+
+
+	if (KEYMANAGER->isOnceKeyDown(VK_LBUTTON))
+	{
+		if (PtInRect(&_homeButton, _ptMouse))
+		{
+			SCENEMANAGER->changeScene("∏ﬁ¿Œ∏ﬁ¥∫æ¿");
+		}
+	}
+}
+
+
 void MapToolScene::render()	
 {
+	int rangeX = WINSIZEX / TILESIZE - 3;
+	int rangeY = WINSIZEY / TILESIZE;
+
+	for (int i = 1; i <= rangeX; i++) {
+		MoveToEx(getMemDC(), i * TILESIZE, TILESIZE ,NULL);
+		LineTo(getMemDC(), i * TILESIZE, rangeY * TILESIZE);
+	}
+	for (int i = 1; i <= rangeY; i++) {
+		MoveToEx(getMemDC(), TILESIZE, i * TILESIZE, NULL);
+		LineTo(getMemDC(), rangeX * TILESIZE, i * TILESIZE);
+	}
+
+
+
+
+	Rectangle(getMemDC(), _homeButton.left, _homeButton.top, _homeButton.right, _homeButton.bottom);
+
 	//test~
-	TextOut(getMemDC(), 100, 100, "∏ ≈¯æ¿", strlen("∏ ≈¯æ¿"));
+	TextOut(getMemDC(), WINSIZEX - 50, 5, "∏ ≈¯æ¿", strlen("∏ ≈¯æ¿"));
+	TextOut(getMemDC(), WINSIZEX - 70, WINSIZEY - 25, "µπæ∆∞°±‚", strlen("µπæ∆∞°±‚"));
 	//~test
 }
--- TeamProj_PixelDungeon/MapToolScene.h
+++ TeamProj_PixelDungeon/MapToolScene.h
@@ -1,14 +1,38 @@
 #pragma once
 #include "gameNode.h"
+
+#define TILESIZE 32
+
+
+
+
+
 class MapToolScene : public gameNode
 {
 private:
+	vector<int> intd;
+
+	
+	vector<RECT> _tileButton;
+	RECT _homeButton;
+
+
+
+
+	
+
+
+	int _cameraX, _cameraY;
+
 
 public:
 	HRESULT init();
 	void release();
 	void update();
 	void render();
+	
+
+	void buttonInput();
 
 	MapToolScene();
 	~MapToolScene();
