74a69bbd9ae1a8d9dd57bbba409c4159b6e90af7
YOOJOOWON <sauna59479@gmail.com>
2017-11-21 00:19:57 +0900

    맵툴 제작중...2

--- TeamProj_PixelDungeon/MapToolScene.cpp
+++ TeamProj_PixelDungeon/MapToolScene.cpp
@@ -12,64 +12,159 @@ MapToolScene::~MapToolScene()
 }
 HRESULT MapToolScene::init()
 {
-	IMAGEMANAGER->addFrameImage("mapTiles", "tile.bmp", 0, 0, 512, 288, 16, 9, true, RGB(255, 0, 255));
+	_paletteImg = IMAGEMANAGER->addFrameImage("mapTiles", "Img/Map/tile.bmp", 0, 0, 512, 288, 16, 9, true, RGB(255, 0, 255));
+
+	//Ä«¸Þ¶ó
+	_cameraX = _cameraY = 0;
+
+	//¸Ê rect
+	for (int i = 0; i < GRIDNUM; i++) {
+		int x = (i % GRIDX + 1) * TILESIZE;
+		int y = (i / GRIDX + 1) * TILESIZE;
+		_mapRect[i].rc = RectMake(x, y, TILESIZE, TILESIZE);
+	}
+	
+	int palPosX = _mapRect[GRIDY].rc.right;
+	int palPosY = _mapRect[GRIDY].rc.top;
+	
+	//ÆÈ·¹Æ® rect
+	for (int i = 0; i < PALETTENUM; i++) {
+		int x = (i % PALETTEX);
+		int y = (i / PALETTEX);
+		_paletRect[i].x = x;
+		_paletRect[i].y = y;
+		_paletRect[i].rc = RectMake((x + 2) * TILESIZE + palPosX, y * TILESIZE + palPosY, TILESIZE, TILESIZE);
+	}
 
 
 
-
-	_homeButton = RectMake(WINSIZEX - 120, WINSIZEY - 60, WINSIZEX - 20, WINSIZEY - 20);
-
+	//¹öÆ°
+	_buttonRect[0] = RectMake(WINSIZEX - 85, WINSIZEY - 40, WINSIZEX - 20, WINSIZEY - 20);
+	
+	paletteSetup();
 	return S_OK;
 }
 void MapToolScene::release()
 {
 
 }
+
+void MapToolScene::paletteSetup()
+{
+	for (int i = 0; i < _paletteImg->getMaxFrameY();  i++) {
+		for (int j = 0; j < _paletteImg->getMaxFrameX(); j++) {
+			tagTile palTile;
+			palTile.img = _paletteImg;
+			palTile.sourX = j;
+			palTile.sourY = i;
+
+			palTile.destX = (j + i * _paletteImg->getMaxFrameX()) % PALETTEX;
+			palTile.destY = (j + i * _paletteImg->getMaxFrameX()) / PALETTEX;
+			_paletTile.push_back(palTile);
+		}		
+	}
+
+
+}
+
+
 void MapToolScene::update()	
 {
-
-
-
-
+	
+	buttonInput();
 }
 
 
 
 void MapToolScene::buttonInput()
 {
+	
 
 
+	for (int i = 0; i < GRIDNUM; i++) {
+		if (PtInRect(&_mapRect[i].rc, _ptMouse)) {
+		
+		//	if (KEYMANAGER->isStayKeyDown(VK_LBUTTON) && _mapSelected.size != 0) {
+		//		if (_sele 
+		//	}
 
+			if (KEYMANAGER->isStayKeyDown(VK_LCONTROL) && KEYMANAGER->isOnceKeyDown(VK_LBUTTON)) {
+				_mapSelected.push_back(_mapRect[i]);
+				break;
+			}
+			
+			else if (KEYMANAGER->isOnceKeyDown(VK_LBUTTON)) {
+				_mapSelected.clear();
+				_mapSelected.push_back(_mapRect[i]);
+				break;
+			}
 
-	if (KEYMANAGER->isOnceKeyDown(VK_LBUTTON))
-	{
-		if (PtInRect(&_homeButton, _ptMouse))
+					
+		}		
+	}
+
+	for (int i = 0; i < PALETTENUM; i++) {
+		if (PtInRect(&_paletRect[i].rc, _ptMouse)) {
+			if (KEYMANAGER->isStayKeyDown(VK_LCONTROL) && KEYMANAGER->isOnceKeyDown(VK_LBUTTON)) {
+				_paletSelected.push_back(_paletRect[i]);
+				break;
+			}
+
+			else if (KEYMANAGER->isOnceKeyDown(VK_LBUTTON)) {
+				_paletSelected.clear();
+				_paletSelected.push_back(_paletRect[i]);
+				break;
+			}
+
+		}
+	}
+
+	for (int i = 0; i < BUTTONNUM; i++) {
+		if (PtInRect(&_buttonRect[i], _ptMouse))
 		{
-			SCENEMANAGER->changeScene("¸ÞÀÎ¸Þ´º¾À");
+			if (KEYMANAGER->isOnceKeyUp(VK_LBUTTON))
+			{
+				SCENEMANAGER->changeScene("¸ÞÀÎ¸Þ´º¾À");				
+			}
 		}
 	}
 }
 
 
 void MapToolScene::render()	
-{
-	int rangeX = WINSIZEX / TILESIZE - 3;
-	int rangeY = WINSIZEY / TILESIZE;
-
-	for (int i = 1; i <= rangeX; i++) {
-		MoveToEx(getMemDC(), i * TILESIZE, TILESIZE ,NULL);
-		LineTo(getMemDC(), i * TILESIZE, rangeY * TILESIZE);
+{	
+	for (int i = 0; i < GRIDNUM; i++) {
+		Rectangle(getMemDC(), _mapRect[i].rc.left, _mapRect[i].rc.top, _mapRect[i].rc.right, _mapRect[i].rc.bottom);
 	}
-	for (int i = 1; i <= rangeY; i++) {
-		MoveToEx(getMemDC(), TILESIZE, i * TILESIZE, NULL);
-		LineTo(getMemDC(), rangeX * TILESIZE, i * TILESIZE);
+	for (int i = 0; i < PALETTENUM; i++) {
+		Rectangle(getMemDC(), _paletRect[i].rc.left, _paletRect[i].rc.top, _paletRect[i].rc.right, _paletRect[i].rc.bottom);
 	}
 
+	
+
+	for (int i = 0; i < PALETTENUM; i++) {
+		if (i >= _paletTile.size()) break;
+		
+		int destX = _paletRect[_paletTile[i].destX + _paletTile[i].destY * PALETTEX].rc.left;
+		int destY = _paletRect[_paletTile[i].destX + _paletTile[i].destY * PALETTEX].rc.top;
+
+		_paletTile[i].img->frameRender(getMemDC(), destX, destY, _paletTile[i].sourX, _paletTile[i].sourY);
+	}
 
 
+	for (int i = 0; i < _mapSelected.size(); i++) {
+		HBRUSH color = CreateSolidBrush(RGB(0, 0, 255));
+		FillRect(getMemDC(), &_mapSelected[i].rc, color);	
+	}
+	
+	for (int i = 0; i < _paletSelected.size(); i++) {
+		HBRUSH color = CreateSolidBrush(RGB(255, 0, 255));
+		FillRect(getMemDC(), &_paletSelected[i].rc, color);
+	}
 
-	Rectangle(getMemDC(), _homeButton.left, _homeButton.top, _homeButton.right, _homeButton.bottom);
-
+	for (int i = 0; i < BUTTONNUM; i++){
+		Rectangle(getMemDC(), _buttonRect[i].left, _buttonRect[i].top, _buttonRect[i].right, _buttonRect[i].bottom);
+	}
 	//test~
 	TextOut(getMemDC(), WINSIZEX - 50, 5, "¸ÊÅø¾À", strlen("¸ÊÅø¾À"));
 	TextOut(getMemDC(), WINSIZEX - 70, WINSIZEY - 25, "µ¹¾Æ°¡±â", strlen("µ¹¾Æ°¡±â"));
--- TeamProj_PixelDungeon/MapToolScene.h
+++ TeamProj_PixelDungeon/MapToolScene.h
@@ -3,27 +3,67 @@
 
 #define TILESIZE 32
 
+#define GRIDX (int)(WINSIZEX / TILESIZE - 7)
+#define GRIDY (int)(WINSIZEY / TILESIZE - 2)
+#define GRIDNUM GRIDX * GRIDY
+
+#define PALETTEX 4
+#define PALETTEY 13
+#define PALETTENUM PALETTEX * PALETTEY
 
 
+#define BUTTONNUM 10
 
+struct tagTile {
+	image* img; //받아올 이미지
+	int sourX, sourY; //받아올 타일 좌표값
+	int destX, destY; //뿌릴 타일 좌표값
+};
+
+struct tagGrid {
+	int x, y;
+	RECT rc;
+};
 
 class MapToolScene : public gameNode
 {
-private:
-	vector<int> intd;
+	typedef vector<tagGrid> _vGrid;
+	typedef vector<tagTile> _vTile;
 
+private:
+	bool _leftClicking;
+
+
+	image* _paletteImg;
+
+	
+
+	_vTile _mapTile; // 맵에 깔아놓은 타일
+	_vGrid _mapSelected; //선택한 맵 그리드
+	tagGrid _mapRect[GRIDNUM]; //맵 전체 그리드 rect
+	
+	
+	_vTile _paletTile; // 팔레트에 깔아놓은 타일
+	_vGrid _paletSelected; //선택한 팔레트 그리드
+	tagGrid _paletRect[PALETTENUM]; //팔레트 전체 그리드 rect
+	
+	
+
+
+
+
+	_vTile _copyTile;
+		
+	
 	
 	vector<RECT> _tileButton;
-	RECT _homeButton;
-
-
-
-
 	
+	RECT _buttonRect[BUTTONNUM];
+	
+		
 
 
 	int _cameraX, _cameraY;
-
 
 public:
 	HRESULT init();
@@ -31,6 +71,8 @@ public:
 	void update();
 	void render();
 	
+	void paletteSetup();
+
 
 	void buttonInput();
 
